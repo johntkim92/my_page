81ee86e910d9f067c7fa5d1e9f8ff0993552e359
John Kim <johntkim92@gmail.com>
2015-09-24 14:59:26 -0400

    reset+wins style, resetClick function

--- tic_tac_toe/tictactoe.js
+++ tic_tac_toe/tictactoe.js
@@ -15,5 +15,17 @@ var player1 = {
 var play2 = {
 
 };
+var reset = {
+  resetClick: function () {
+    var reset = $('.reset');
+    var p1Wins = $('.p1');
+    var p2Wins = $('.p2');
+    reset.on('click', function (e) {
+      p1Wins.text('Player 1 Wins: 0');
+      p2Wins.text('Player 2 Wins: 0');
+    });
+  },
+  
+};
 
 game.boardClick();
