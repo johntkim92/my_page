6ffdc52a046e5119f4c66abd916213370d0048a5
John Kim <johntkim92@gmail.com>
2015-09-24 15:12:27 -0400

    resetClick, clears board

--- tic_tac_toe/tictactoe.js
+++ tic_tac_toe/tictactoe.js
@@ -20,12 +20,15 @@ var reset = {
     var reset = $('.reset');
     var p1Wins = $('.p1');
     var p2Wins = $('.p2');
+    var cells = $('.cell');
     reset.on('click', function (e) {
       p1Wins.text('Player 1 Wins: 0');
       p2Wins.text('Player 2 Wins: 0');
+      cells.text('');
     });
   },
-  
+
 };
 
 game.boardClick();
+reset.resetClick();
